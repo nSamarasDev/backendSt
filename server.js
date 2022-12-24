3a82d0c9b76bc8213a5ab2382f4691e74d75c160
“[Nicholas <n.samaras4@outlook.com>
2022-12-24 16:36:39 -0500

    server.js updates

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const logger = require('./middleware/logger')
+
+// Route variables 
+const profile = require('./routes/profile')
 
 const app = express()
 
+// Dev logging middleware
+app.use(logger)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+// Mount Routers
+app.use('/api/v1/profile', profile)
+
 // env vars 
 dotenv.config({ path: './config/config.env' })
+
+
 
 const PORT = process.env.PORT || 5000
 
