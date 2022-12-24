d255d90e1cbfd7ddc76f51c37373f6c7bf7d54aa
“[Nicholas <n.samaras4@outlook.com>
2022-12-24 16:33:42 -0500

    profile.js controllers added

---
+++ controllers/profile.js
@@ -0,0 +1,38 @@
+
+// @des     Get all profiles
+// @route   GET /api/v1/profile
+// @access  Public
+exports.getProfiles = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'Show all profiles', hello: req.hello })
+}
+
+// @des     Get single profile
+// @route   GET /api/v1/profile/:id
+// @access  Public
+exports.getProfile = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Get profile ${req.params.id}` })
+}
+
+// @des     Create a profile
+// @route   POST /api/v1/profile
+// @access  Private
+exports.createProfile = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'Create new profile' })
+}
+
+// @des     Update a profile
+// @route   PUT /api/v1/profile/:id
+// @access  Private
+exports.updateProfile = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Update profile ${req.params.id}` })
+}
+
+// @des     Delete a profile
+// @route   DELETE /api/v1/profile/:id
+// @access  Private
+exports.deleteProfile = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Update profile ${req.params.id}` })
+}
+
+
+
